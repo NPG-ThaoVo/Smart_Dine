7b521f1b5af456324a7f4101c5439d905df9c17d
lalabinbin <binhminh2711@gmail.com>
2026-01-11 21:29:36 +0700

    feat: add PWA support and update dependencies
    - Added vite-plugin-pwa to enable Progressive Web App features.
    - Updated package.json to include vite-plugin-pwa.
    - Added logo-app.png to public and assets directories.
    - Updated vite.config.js to configure PWA settings and include app icons.

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,21 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", // Tự động cập nhật khi có nội dung mới
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "SmartDine - Food Ordering App",
+        short_name: "SmartDine",
+        description:
+          "Ứng dụng đặt món ăn nhanh chóng bằng QR, xem menu, gọi món và theo dõi trạng thái đơn hàng trực tiếp tại bàn.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo-app.png", // Bạn phải chuẩn bị icon này trong thư mục public
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo-app.png", // Bạn phải chuẩn bị icon này trong thư mục public
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: true,
     port: 3000,
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
     },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
