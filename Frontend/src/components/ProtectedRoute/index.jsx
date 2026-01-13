3434da8bc50e023e199ccbacd4886d0646cd7796
Nghia1245654 <kingwinlata@gmail.com>
2026-01-13 22:32:32 +0700

    feat: clean up ProtectedRoute component by removing unnecessary whitespace

--- Frontend/src/components/ProtectedRoute/index.jsx
+++ Frontend/src/components/ProtectedRoute/index.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { useContext } from "react";
 import AuthContext from "@/contexts/AuthContext";
 import { Navigate } from "react-router-dom";
-
 const ProtectedRoute = ({ children }) => {
   const { userInfo } = useContext(AuthContext);
   if (!userInfo) {
     return <Navigate to="/admin/login" />;
   }
-
   return children;
 };
 
