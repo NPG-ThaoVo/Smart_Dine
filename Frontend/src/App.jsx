8ab0f881da58e87bae4f5558fcb09e3fefd9125a
Nghia1245654 <kingwinlata@gmail.com>
2026-01-13 21:56:38 +0700

    feat: add authentication context and protected routes, integrate Google login functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,28 +12,32 @@ import OrderManagementPage from "./pages/OrderManagement";
 import DashboardPage from "./pages/DashboardPage";
 import BillingPage from "./pages/BillingPage";
 import NotificationManagementPage from "./pages/notificationManagement";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./contexts/authContext";
 function App() {
   return (
     <BrowserRouter>
       <Toaster position="top-right" reverseOrder={false} />
+      <AuthProvider>
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/admin/login" element={<AdminLoginPage />} />
         <Route path="/order/1/item/item-1" element={<DetailsPage />} />
         <Route path="/smartdine" element={<SmartDineLanding />} />
         
-
+          
         <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<Navigate to="dashboard" replace />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="table-management" element={<TableManagement />} />
-          <Route path="menu" element={<MenuAdminPage />} />
-          <Route path="order-management" element={<OrderManagementPage />} />
-          <Route path="billing" element={<BillingPage />} />
-          <Route path="notification-management" element={<NotificationManagementPage />} />
+          <Route index element={<ProtectedRoute><Navigate to="dashboard" replace /></ProtectedRoute>} />
+          <Route path="dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+          <Route path="table-management" element={<ProtectedRoute><TableManagement /></ProtectedRoute>} />
+          <Route path="menu" element={<ProtectedRoute><MenuAdminPage /></ProtectedRoute>} />
+          <Route path="order-management" element={<ProtectedRoute><OrderManagementPage /></ProtectedRoute>} />
+          <Route path="billing" element={<ProtectedRoute><BillingPage /></ProtectedRoute>} />
+          <Route path="notification-management" element={<ProtectedRoute><NotificationManagementPage /></ProtectedRoute>} />
         </Route>
         <Route path="/order/confirm" element={<ConfirmPage />} />
       </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
---
+++ Frontend/src/components/ProtectedRoute/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useContext } from "react";
+import AuthContext from "@/contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const { userInfo } = useContext(AuthContext);
+  if (!userInfo) {
+    return <Navigate to="/admin/login" />;
+  }
+console.log("AuthContext =", useContext(AuthContext));
+
+  return children;
+};
+
+export default ProtectedRoute;
