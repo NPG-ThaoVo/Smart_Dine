c756dd7f50ce15e0104a587b041013f5375213b7
NPG-ThaoVo <45417567+NPG-ThaoVo@users.noreply.github.com>
2026-01-17 17:43:57 +0700

    Rename import for AdminLoginPage to lowercase

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import AdminLoginPage from "./pages/AdminLoginPage";
+import AdminLoginPage from "./pages/adminLoginPage";
 import DetailsPage from "./pages/DetailsPage";
 import Menu from "./pages/Menu";
 import SmartDineLanding from "./pages/Smartdine-Landing-Page";
